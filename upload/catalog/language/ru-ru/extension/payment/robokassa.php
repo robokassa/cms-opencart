dca4de4e67c29ab2705f754527e40eead4d7edeb
UranusBurn <rdetels@robokassa.ru>
2020-02-28 14:21:04 +0300

    Метка official_opencart
    Signed-off-by: UranusBurn <rdetels@robokassa.ru>

--- upload/catalog/language/ru-ru/extension/payment/robokassa.php
+++ upload/catalog/language/ru-ru/extension/payment/robokassa.php
@@ -1,4 +1,4 @@
 <?php
 // Text
-$_['text_title']       = 'Робокасса <img src="image/payment/robokassa.png" alt="" style="max-width: 150px;">';
-$_['text_description']       = 'Робокасса <img src="image/payment/robokassa.png" alt="" style="max-width: 150px;">';
+$_['text_title']       = 'Робокака <img src="image/payment/robokassa.png" alt="" style="max-width: 150px;">';
+$_['text_description']       = 'Робокака <img src="image/payment/robokassa.png" alt="" style="max-width: 150px;">';
